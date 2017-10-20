82a9d54fefb88b7724651f330450b07fa41a3b18
Marek <mareklani@gmail.com>
2017-10-20 15:04:47 +0200

    Initial Commit

---
+++ CodingSchoolAppInsights/CodingSchoolAppInsights/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodingSchoolAppInsights.Startup))]
+namespace CodingSchoolAppInsights
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
